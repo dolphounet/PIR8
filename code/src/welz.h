cfa0d1b9b25588179ab1e5564f3818e8674a5394
karen19 <mendoza.s1503@gmail.com>
2024-05-06 22:07:17 +0200

    new

--- code/src/welz.h
+++ code/src/welz.h
@@ -7,8 +7,8 @@
 #include <math.h>
 #include "structures.h"
 #include "global.h"
+
 double distance(POINT p1, POINT p2) ;
-int pointInsideCircle(POINT p, CERCLE c) ;
 CERCLE algo_welz(POINT* tab, int n);
 
 
